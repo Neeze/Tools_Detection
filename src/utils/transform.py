7a3d7932df3e40900fd5615724d5d37a77537b3e
Neeze <phanvanhainam.2019@gmail.com>
2024-06-10 16:41:51 +0700

    Add support for bounding box transformations in ImageTransformer

--- src/utils/transform.py
+++ src/utils/transform.py
@@ -1,5 +1,7 @@
 import torch
 from torchvision import transforms
+from typing import Tuple
+
 
 class ImageTransformer:
     def __init__(self, config: dict):
@@ -8,32 +10,41 @@ class ImageTransformer:
         Args:
             config: A dictionary containing configuration parameters.
         """
-        self.train_transform = transforms.Compose([
-            transforms.ToPILImage(),
-            transforms.Resize((config["img_height"], config["img_width"])),
-            transforms.RandomHorizontalFlip(), 
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-        ])
+        self.config = config
 
-        self.test_transform = transforms.Compose([
+        self.transform = transforms.Compose([
             transforms.ToPILImage(),
             transforms.Resize((config["img_height"], config["img_width"])),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
 
-    def __call__(self, image: torch.Tensor, is_train: bool) -> torch.Tensor:
-        """Applies the appropriate transformation to the image.
+        # Augmentation specifically for training
+        self.train_augmentation = transforms.RandomHorizontalFlip()
+
+    def __call__(self, image: torch.Tensor, target: dict, is_train: bool) -> Tuple[torch.Tensor, dict]:
+        """Transforms image and bounding boxes.
 
         Args:
             image: The image tensor to be transformed.
+            target: A dictionary containing the target data (e.g., bounding boxes).
             is_train: A flag indicating whether the image is for training (True) or testing (False).
 
         Returns:
-            The transformed image tensor.
+            The transformed image tensor and the transformed target dictionary.
         """
-        if is_train:
-            return self.train_transform(image)
-        else:
-            return self.test_transform(image)
+
+        _, witdh, height = image.shape
+
+        # Apply common transformations (resize, to tensor, normalize)
+        image = self.transform(image)
+        boxes = target["boxes"]
+
+            
+        # Rescale the coordinates of the bounding boxes
+        transformed_size = torch.tensor(image.shape[1::], dtype=torch.float32)  # [h, w]
+        # boxes = torch.tensor(target["boxes"], dtype=torch.float32)  # Convert directly from target dict
+        boxes[:, [0, 2]] = boxes[:, [0, 2]] * transformed_size[1] / witdh # Normalize x coordinates
+        boxes[:, [1, 3]] = boxes[:, [1, 3]] * transformed_size[0] / height # Normalize y coordinates
+    
+        return image, target
